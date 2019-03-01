30bfb3525d046597b94c252fc8b7a07efa492cc8
Mina Ferizbegovic <minafe@UG.KTH.SE>
2019-03-01 21:00:16 +0100

    Separating function implementation from classes

--- controller_structure.m
+++ controller_structure.m
@@ -15,27 +15,5 @@ classdef controller_structure
             obj.Z = Z;
             obj.X = X;
         end
-        function con = get_nominal_controller(obj, model_app, system_real, Tf)
-            % designed using initial uncertainty
-            assert(Tf>0); %otherwise cost isn't meaningful
-            tau = sdpvar(1);
-            Qcl = model_app.Qcl;
-            Rcl = model_app.Rcl;
-            
-            C = [Qcl*obj.W; Rcl*obj.Z'];
-            S = [obj.X C; C' obj.W]; %the cost
-            Objective = trace(obj.X);
-            Constraints = [obj.T1-tau*obj.T2>=0, obj.W>=0,S>=0, tau>=0];
-            ops = sdpsettings('solver','mosek','verbose',0);
-            sol = optimize(Constraints,Objective, ops);
-            
-            K0 = double(obj.Z)'/(double(obj.W));
-            cost_old = double(Objective);
-            Wnom = double(obj.W);
-            cost_new = calculate_cost(K0, system_real, Tf);
-            
-            con = controller_ss(K0, Wnom, cost_old, cost_new);
-                
-        end
     end
 end
--- system_ss.m
+++ system_ss.m
@@ -19,22 +19,5 @@ classdef system_ss
             Nx = size(obj.A,1);
             Nu = size(obj.B,2);
         end
-        function cost = calculate_cost(Ksim, model, Tf)
-            assert(~any(isnan(Ksim(:)))); %otherwise controller doesn't exist
-            assert(Tf>0); %otherwise cost isn't meaningful
-            A = model.A;
-            B = model.B;
-            [Nx, Nu] = get_state_size(model);
-            W = model.sigma_w*randn(Nx,Tf);
-            xs = zeros(Nx,Tf);
-            us = zeros(Nu,Tf);
-            c = 0;
-            for t = 1:Tf
-                us(:,t) = Ksim*xs(:,t);
-                xs(:,t+1) = A*xs(:,t) + B*us(:,t) + W(:,t);
-                c = c + xs(:,t)'*model.Qcl'*model.Qcl*xs(:,t) + us(:,t)'*model.Rcl'*model.Rcl*us(:,t);
-            end
-            cost = c/Tf;
-        end
     end
 end
