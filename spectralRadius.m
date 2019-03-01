f96336611e0c99724812401fe6a63214b4128c07
Mina Ferizbegovic <minafe@UG.KTH.SE>
2019-03-01 20:50:33 +0100

    Assertion for spectral radius

--- spectralRadius.m
+++ spectralRadius.m
@@ -1,8 +1,7 @@
 function s = spectralRadius(A)
 
-if sum(sum(isnan(A)))
-    s = nan;
-else
-    tmp = eig(A);
-    s = max(sqrt(diag(tmp*tmp')));
+assert(~any(isnan(A(:))));
+tmp = eig(A);
+s = max(sqrt(diag(tmp*tmp')));
+
 end
